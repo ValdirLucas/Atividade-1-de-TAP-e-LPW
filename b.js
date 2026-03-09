2c5ab2ca5ea18b10834c181ffe8e92cb47a7db68
Lucas <lucasaraujo5623@gmail.com>
2026-03-09 09:35:48 -0400

    Atualizar o b.js

--- b.js
+++ b.js
@@ -1,6 +1,6 @@
-var Valor = Number(prompt("Digite o Valor! "))
-var Numcliente = Number(prompt("Quantos cliente são? "))
-var cliente = Valor / 3;
+let Valor = Number(prompt("Digite o Valor! "))
+let Numcliente = Number(prompt("Quantos cliente são? "))
+let cliente = Valor / 3;
 alert(" O valor da conta foi R$: " + Valor + 
 "\n número de cliente é: " + Numcliente +
 "\n O valor por cliente é: " + cliente)
