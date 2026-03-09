4c13b730d3b21b90163b52a8ba59597dbd4bdd6c
Lucas <lucasaraujo5623@gmail.com>
2026-03-09 09:44:00 -0400

    Update c.js

--- c.js
+++ c.js
@@ -1,5 +1,5 @@
-var preco = Number(prompt("Digite o preço do produto!"))
-var vista = (preco * 10) / 100
-var resultado = preco - vista
-var parcela = (preco / 3)
+let preco = Number(prompt("Digite o preço do produto!"))
+let vista = (preco * 10) / 100
+let resultado = preco - vista
+let parcela = (preco / 3)
 alert(" o valor do produto é: " + preco + "\n a vista vai ser: " + resultado + ("\n os 3x: " + parcela))
